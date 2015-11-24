c05e2aa9b4615d4fa6aa03f06e8b374adb39ac4c
BrunoAl <brunoalnascimento@gmail.com>
2015-11-23 22:58:08 -0200

    all done!

--- main.c
+++ main.c
@@ -30,12 +30,6 @@ int main()
         	return 0;
 	}
 
-    if(menuOption == 1) {
-    	//loadTableSize(&x, &y);
-    }
-    if(menuOption == 0){
-			
-    }
 	
     gameMenu(x, y, currentGen, table);
 		printf("\n\nFechando o Jogo...\n\n");
